d221bea5535e88b7195898d44310527e688b8f3d
flbaue <flbaue@posteo.de>
2014-10-24 19:55:41 +0200

    changed project to maven

---
+++ praktikum1/src/main/java/aufgabe1/algorithms/BreadthFirstSearch.java
@@ -0,0 +1,9 @@
+package aufgabe1.algorithms;
+
+/**
+ * Created by flbaue on 24.10.14.
+ */
+public class BreadthFirstSearch {
+
+    
+}
---
+++ praktikum1/src/test/java/aufgabe1/io/GraphIOTest.java
@@ -0,0 +1,31 @@
+package aufgabe1.io;
+
+import aufgabe1.Vertex;
+import org.jgrapht.Graph;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.junit.Test;
+
+import java.io.File;
+import java.net.URL;
+
+import static org.junit.Assert.*;
+
+public class GraphIOTest {
+
+    @Test
+    public void testReadGraphFromFile() throws Exception {
+        URL url = getClass().getResource("/SimpleTestGraph.txt");
+        File file = new File(url.toURI());
+
+        GraphIO graphIO = new GraphIO();
+        Graph<Vertex, DefaultWeightedEdge> graph = graphIO.readGraphFromFile(file);
+
+        assertEquals(2, graph.edgeSet().size());
+        assertEquals(3, graph.vertexSet().size());
+    }
+
+    @Test
+    public void testSaveGraphAsFile() throws Exception {
+
+    }
+}
