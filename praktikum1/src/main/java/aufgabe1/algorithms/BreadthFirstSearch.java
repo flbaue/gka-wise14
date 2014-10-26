4729a58cc2f13834d829918311daf77e6759023b
flbaue <flbaue@posteo.de>
2014-10-26 02:10:10 +0100

    removed directed bfs iterator

--- praktikum1/src/main/java/aufgabe1/algorithms/BreadthFirstSearch.java
+++ praktikum1/src/main/java/aufgabe1/algorithms/BreadthFirstSearch.java
@@ -1,8 +1,6 @@
 package aufgabe1.algorithms;
 
 import aufgabe1.Vertex;
-import aufgabe1.utils.GraphUtils;
-import org.jgrapht.DirectedGraph;
 import org.jgrapht.Graph;
 import org.jgrapht.graph.DefaultWeightedEdge;
 
@@ -15,20 +13,14 @@ public class BreadthFirstSearch implements Iterable<Vertex> {
 
     private final Vertex startVertex;
     private final Graph<Vertex, DefaultWeightedEdge> graph;
-    private final boolean isDrirected;
 
     public BreadthFirstSearch(Graph<Vertex, DefaultWeightedEdge> graph, Vertex startVertex) {
         this.startVertex = startVertex;
         this.graph = graph;
-        this.isDrirected = GraphUtils.isDirectedGraph(graph);
     }
 
     @Override
     public Iterator<Vertex> iterator() {
-        if (isDrirected) {
-            return new DirectedBfsIterator((DirectedGraph<Vertex, DefaultWeightedEdge>) graph, startVertex);
-        } else {
-            return new BfsIterator(graph, startVertex);
-        }
+        return new BfsIterator(graph, startVertex);
     }
 }
