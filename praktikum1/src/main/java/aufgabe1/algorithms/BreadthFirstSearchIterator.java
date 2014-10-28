f7a5b7704a4baba64ea59fb5a92784db2de8ecb9
flbaue <flbaue@posteo.de>
2014-10-28 14:39:40 +0100

    bfs remastered

--- praktikum1/src/main/java/aufgabe1/algorithms/BreadthFirstSearchIterator.java
+++ praktikum1/src/main/java/aufgabe1/algorithms/BreadthFirstSearchIterator.java
@@ -16,92 +16,42 @@ import java.util.LinkedList;
 public class BreadthFirstSearchIterator implements Iterator<Vertex> {
 
     private Deque<Vertex> queue = new LinkedList<>();
-    private Deque<Vertex> neighbors = new LinkedList<>();
-    private Vertex currentVertex;
     private Graph<Vertex, DefaultWeightedEdge> graph;
     private int dereferences = 0;
 
     public BreadthFirstSearchIterator(Graph<Vertex, DefaultWeightedEdge> graph, Vertex startVertex) {
         GraphUtils.removeMarkers(graph);
-        startVertex.setMarker(new Marker(null, 0));
+        startVertex.setMarker(new Marker(startVertex, 0));
         queue.add(startVertex);
         this.graph = graph;
     }
 
     private void findNeighbors(final Vertex vertex) {
-        neighbors = new LinkedList<>();
+        //neighbors = new LinkedList<>();
         dereferences++;
         for (DefaultWeightedEdge e : graph.edgesOf(vertex)) {
             dereferences++;
             Vertex v = graph.getEdgeTarget(e);
-            if ((v.hasMarker() && v.isVisited()) || v.equals(vertex)) {
+            if (v.hasMarker() || v.equals(vertex)) {
                 continue;
             } else if (!v.hasMarker()) {
-                v.setMarker(new Marker(null, 0));
+                v.setMarker(new Marker(vertex, v.getDistance() + 1));
                 queue.add(v);
             }
-            neighbors.add(v);
         }
     }
 
     @Override
     public boolean hasNext() {
         // getting the next vertex to visit
-        if (currentVertex == null && queue.size() > 0) {
-            currentVertex = queue.poll();
-            findNeighbors(currentVertex);
-            return true;
-        }
-
-        // is there a neighbor we haven't visited, or that will be closer now?
-        if (currentVertex != null) {
-            for (Vertex v : neighbors) {
-                if (v.isVisited()) {
-                    continue;
-                }
-                int weight = getEdgeWeight(v);
-                if (v.getPredecessor() == null || v.getDistance() > currentVertex.getDistance() + weight) {
-                    return true;
-                }
-            }
-        }
-
-        // if no neighbor was available, we try the next vertex
-        if (queue.size() > 0) {
-            currentVertex = queue.poll();
-            findNeighbors(currentVertex);
-            return true;
-        }
-
-        // still nothing, than we are done.
-        return false;
-    }
-
-    private int getEdgeWeight(Vertex v) {
-        dereferences++;
-        DefaultWeightedEdge e = graph.getEdge(currentVertex, v);
-        dereferences++;
-        return (int) graph.getEdgeWeight(e);
+        return queue.size() > 0;
     }
 
     @Override
     public Vertex next() {
-        if (!currentVertex.isVisited()) {
-            currentVertex.visit();
-            return currentVertex;
-        }
-
-        if (neighbors.size() > 0) {
-            Vertex n = neighbors.poll();
-            int weight = getEdgeWeight(n);
-            n.setMarker(new Marker(currentVertex, currentVertex.getDistance() + weight));
-            if (neighbors.size() == 0) {
-                currentVertex = null;
-            }
-            return n;
-        }
-
-        return null;
+        Vertex v = queue.poll();
+        findNeighbors(v);
+        return v;
     }
 
     public int getGraphDereferences() {
