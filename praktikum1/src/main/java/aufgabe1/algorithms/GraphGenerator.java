d2e2a62380b71d89f912a238e979b08c77b62efa
Marcel Schlegel <marcel.schlegel@posteo.de>
2014-11-17 21:45:32 +0100

    Added graph generator.

---
+++ praktikum1/src/main/java/aufgabe1/algorithms/GraphGenerator.java
@@ -0,0 +1,98 @@
+package aufgabe1.algorithms;
+
+import aufgabe1.Vertex;
+import org.jgrapht.WeightedGraph;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.DirectedWeightedPseudograph;
+import org.jgrapht.graph.WeightedPseudograph;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * Created by schlegel11 on 17.11.14.
+ */
+public class GraphGenerator {
+
+    private final int edgeCount;
+    private final int vertexCount;
+    private final boolean isDirected;
+    private WeightedGraph<Vertex, DefaultWeightedEdge> graph;
+    private Map<Integer, Vertex> vertices = new HashMap<>();
+
+    public GraphGenerator(int edgeCount, int vertexCount, boolean isDirected) {
+        this.edgeCount = edgeCount;
+        this.vertexCount = vertexCount;
+        this.isDirected = isDirected;
+    }
+
+    public void generate() {
+        createGraph();
+        generateVertices();
+        generateEdges();
+    }
+
+    public void addShortestPath(Collection<Vertex> vertices) {
+        Vertex tempVertex = null;
+        for (Iterator<Vertex> itr = vertices.iterator(); itr.hasNext(); ) {
+            Vertex vertex = itr.next();
+            if (Objects.nonNull(tempVertex)) {
+                graph.setEdgeWeight(graph.addEdge(tempVertex, vertex), 1);
+            }
+            tempVertex = vertex;
+        }
+    }
+
+    public int getEdgeCount() {
+        return edgeCount;
+    }
+
+    public int getVertexCount() {
+        return vertexCount;
+    }
+
+    public boolean isDirected() {
+        return isDirected;
+    }
+
+    public WeightedGraph<Vertex, DefaultWeightedEdge> getGraph() {
+        return graph;
+    }
+
+    private void createGraph() {
+        graph = isDirected() ? new DirectedWeightedPseudograph<>(DefaultWeightedEdge.class) : new WeightedPseudograph<>(DefaultWeightedEdge.class);
+    }
+
+    private void generateVertices() {
+        for (int i = 1; i <= getVertexCount(); i++) {
+            Vertex vertex = new Vertex(String.valueOf(i));
+            graph.addVertex(vertex);
+            vertices.put(i, vertex);
+        }
+    }
+
+    private void generateEdges() {
+        int edgesPerVertex = getEdgeCount() / getVertexCount();
+        int edgesPerVertexRest = getEdgeCount() % getVertexCount();
+        for (int i = 1; i <= getVertexCount(); i++) {
+            for (int j = 0; j < edgesPerVertex; j++) {
+                Vertex sourceVertex = vertices.get(i);
+                graph.setEdgeWeight(graph.addEdge(sourceVertex, getRandomVertex(vertices.values())), getRandomWeight(10, 100));
+            }
+        }
+        for (int i = 0; i < edgesPerVertexRest; i++) {
+            graph.setEdgeWeight(graph.addEdge(getRandomVertex(vertices.values()), getRandomVertex(vertices.values())), getRandomWeight(10, 100));
+        }
+    }
+
+    private Vertex getRandomVertex(Collection<Vertex> collection) {
+        List<Vertex> list = collection.stream().collect(Collectors.toList());
+        Collections.shuffle(list);
+        return list.get(0);
+    }
+
+    private int getRandomWeight(int min, int max) {
+        Random random = new Random();
+        return random.nextInt(max - min) + min;
+    }
+}
---
+++ praktikum1/src/test/java/aufgabe1/algorithms/GraphGeneratorTest.java
@@ -0,0 +1,28 @@
+package aufgabe1.algorithms;
+
+import aufgabe1.Vertex;
+import com.mxgraph.swing.handler.mxVertexHandler;
+import junit.framework.TestCase;
+import org.junit.Assert;
+
+import java.util.Arrays;
+
+public class GraphGeneratorTest extends TestCase {
+
+    public void testGenerate() throws Exception {
+
+        GraphGenerator graphGenerator = new GraphGenerator(5999, 100, true);
+        graphGenerator.generate();
+
+        Assert.assertEquals(5999, graphGenerator.getGraph().edgeSet().size());
+        Assert.assertEquals(100, graphGenerator.getGraph().vertexSet().size());
+
+        //Shortest path with edge weight 1 from 1 -> 20
+        graphGenerator.addShortestPath(Arrays.asList(new Vertex("1"), new Vertex("20")));
+        Assert.assertEquals(6000, graphGenerator.getGraph().edgeSet().size());
+
+        //Shortest path with edge weight 1 from 1 -> 20 -> 15
+        graphGenerator.addShortestPath(Arrays.asList(new Vertex("1"), new Vertex("20"), new Vertex("15")));
+        Assert.assertEquals(6002, graphGenerator.getGraph().edgeSet().size());
+    }
+}
