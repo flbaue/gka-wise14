c0e746812b599923c7bc3cd027572d3f6f75842b
Marcel Schlegel <marcel.schlegel@posteo.de>
2014-12-09 03:53:16 +0100

    Add test

--- praktikum1/src/test/java/aufgabe1/algorithms/FordFulkersonTest.java
+++ praktikum1/src/test/java/aufgabe1/algorithms/FordFulkersonTest.java
@@ -1,11 +1,20 @@
 package aufgabe1.algorithms;
 
 import aufgabe1.Vertex;
+import aufgabe1.io.GraphIO;
 import junit.framework.TestCase;
+import org.jgrapht.Graph;
 import org.jgrapht.graph.DefaultWeightedEdge;
 import org.jgrapht.graph.DirectedWeightedPseudograph;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.File;
+import java.net.URL;
 
 public class FordFulkersonTest extends TestCase {
+
+    @Test
     public void testRun() throws Exception {
         DirectedWeightedPseudograph<Vertex, DefaultWeightedEdge> graph = new DirectedWeightedPseudograph(DefaultWeightedEdge.class);
 
@@ -36,6 +45,51 @@ public class FordFulkersonTest extends TestCase {
         FordFulkerson fordFulkerson = new FordFulkerson(graph);
         fordFulkerson.run(s, t);
 
+        Assert.assertEquals(12, fordFulkerson.getMaxFlow());
+    }
 
+    @Test
+    public void testRun2() throws Exception {
+        DirectedWeightedPseudograph<Vertex, DefaultWeightedEdge> graph = new DirectedWeightedPseudograph(DefaultWeightedEdge.class);
+
+        Vertex s = new Vertex("s");
+        graph.addVertex(s);
+        Vertex u = new Vertex("u");
+        graph.addVertex(u);
+        Vertex v = new Vertex("v");
+        graph.addVertex(v);
+        Vertex t = new Vertex("t");
+        graph.addVertex(t);
+
+
+        graph.setEdgeWeight(graph.addEdge(s, u), 10);
+        graph.setEdgeWeight(graph.addEdge(s, u), 10);
+        graph.setEdgeWeight(graph.addEdge(s, v), 10);
+        graph.setEdgeWeight(graph.addEdge(u, t), 10);
+        graph.setEdgeWeight(graph.addEdge(u, v), 30);
+        graph.setEdgeWeight(graph.addEdge(v, t), 20);
+
+
+        FordFulkerson fordFulkerson = new FordFulkerson(graph);
+        fordFulkerson.run(s, t);
+
+        Assert.assertEquals(30, fordFulkerson.getMaxFlow());
+    }
+
+    @Test
+    public void testRun3() throws Exception {
+        GraphIO graphIO = new GraphIO();
+        URL url = getClass().getResource("/graph4.gka.txt");
+        File file = new File(url.toURI());
+
+        Graph<Vertex, DefaultWeightedEdge> graph = graphIO.readGraphFromFile(file);
+
+        Vertex q = graph.vertexSet().stream().filter(vertex -> vertex.getName().equals("q")).findFirst().get();
+        Vertex s = graph.vertexSet().stream().filter(vertex -> vertex.getName().equals("s")).findFirst().get();
+
+        FordFulkerson fordFulkerson = new FordFulkerson(graph);
+        fordFulkerson.run(q, s);
+
+        Assert.assertEquals(26, fordFulkerson.getMaxFlow());
     }
 }
